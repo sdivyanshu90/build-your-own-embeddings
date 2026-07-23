d10580b38b46e4fd51fa428b502a2cdc160a89b6
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-07-23 15:57:00 +0530

    Implement embedding model component: constants

---
+++ src/embedding_model/constants.py
@@ -0,0 +1,7 @@
+"""Shared format and package versions."""
+
+__version__ = "0.1.0"
+ARTIFACT_SCHEMA_VERSION = 1
+CHECKPOINT_SCHEMA_VERSION = 1
+INDEX_SCHEMA_VERSION = 1
+DEFAULT_MODEL_NAME = "local-transformer-embedding"
